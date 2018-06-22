5d8b5d0a6e5909c5dc8aff424c62c8534bcc1118
ThienVuLe <thienvurock@gmail.com>
2018-06-22 10:04:45 +0700

    First commit: Finishing Flash-Chat application

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-
+import Firebase
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
 
@@ -29,10 +30,19 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject) {
         
-
+        SVProgressHUD.show()
         
         //TODO: Set up a new user on our Firbase database
-        
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!)
+        { (user, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Successful Register")
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
 
         
